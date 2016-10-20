7647bc2cb4bd41eb3081bfddba7937ea62000b17
Eyck Jentzsch <eyck@minres.com>
2016-10-20 19:29:28 +0000

    Updated license headers and project settings

--- scv4tlm/tlm_rec_target_socket.h
+++ scv4tlm/tlm_rec_target_socket.h
@@ -1,9 +1,18 @@
-/*
- * tlm_rec_target_socket.h
+/*******************************************************************************
+ * Copyright 2016 MINRES Technologies GmbH
  *
- *  Created on: 08.11.2015
- *      Author: eyck
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 #ifndef TLM_REC_TARGET_SOCKET_H_
 #define TLM_REC_TARGET_SOCKET_H_
