68e37c26ff7666fa36d4871796728bca060a4c3d
Eyck Jentzsch <eyck@minres.com>
2018-10-04 10:08:55 +0200

    Fixed namespace for make_unique

--- incl/scc/utilities.h
+++ incl/scc/utilities.h
@@ -34,9 +34,11 @@
 #include <locale>
 
 #if __cplusplus < 201402L
+namespace std {
 template<typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args){
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
 }
 #endif
 
