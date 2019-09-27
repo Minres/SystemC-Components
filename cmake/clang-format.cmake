c08c553e9e2715d5e7e4f4d7f66250ce6862fa81
Eyck Jentzsch <eyckj@arteris.com>
2019-09-27 18:20:41 +0200

    Build system fixes
    * added a FindPackage module for clang-format
    * fixed library dependencies

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,20 +1,44 @@
 # additional target to perform clang-format run, requires clang-format
 
-# get all project files
-file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
-foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
-    foreach(PROJECT_TRDPARTY_DIR  ${PROJECT_3PARTY_DIRS})
-        string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_EXTERNAL_DIR_FOUND)
-        if (NOT ${PROJECT_EXTERNAL_DIR_FOUND} EQUAL -1)
-            #message( STATUS "Skipping ${SOURCE_FILE} for clang-format." )
+set(CLANG_FORMAT_CXX_FILE_EXTENSIONS ${CLANG_FORMAT_CXX_FILE_EXTENSIONS} *.cpp *.h *.cxx *.hxx *.hpp *.cc *.ipp)
+file(GLOB_RECURSE ALL_SOURCE_FILES ${CLANG_FORMAT_CXX_FILE_EXTENSIONS})
+
+# Don't include some common build folders
+set(CLANG_FORMAT_EXCLUDE_PATTERNS ${CLANG_FORMAT_EXCLUDE_PATTERNS} "/CMakeFiles/" "cmake")
+
+# get all project files file
+foreach (SOURCE_FILE ${ALL_SOURCE_FILES}) 
+    foreach (EXCLUDE_PATTERN ${CLANG_FORMAT_EXCLUDE_PATTERNS})
+        string(FIND ${SOURCE_FILE} ${EXCLUDE_PATTERN} EXCLUDE_FOUND) 
+        if (NOT ${EXCLUDE_FOUND} EQUAL -1) 
             list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
-        endif ()
-    endforeach()
+        endif () 
+    endforeach ()
 endforeach ()
 
-add_custom_target(
-        clangformat
-        COMMAND clang-format
+add_custom_target(clangformat
+    COMMENT "Running clang-format to change files"
+        COMMAND ${CLANG_FORMAT_BIN}
         -i
         ${ALL_SOURCE_FILES}
+)
+
+add_custom_target(format
+    COMMENT "Running clang-format to change files"
+    COMMAND ${CLANG_FORMAT_BIN}
+    -style=file
+    -i
+    ${ALL_SOURCE_FILES}
+)
+
+
+add_custom_target(format-check
+    COMMENT "Checking clang-format changes"
+    # Use ! to negate the result for correct output
+    COMMAND !
+    ${CLANG_FORMAT_BIN}
+    -style=file
+    -output-replacements-xml
+    ${ALL_SOURCE_FILES}
+    | grep -q "replacement offset" 
 )
