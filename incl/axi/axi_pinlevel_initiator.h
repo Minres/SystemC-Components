4b5f74c069802e86ca180d4b33bf453c868b19b8
Stas <stas@minres.com>
2021-03-08 12:06:20 +0100

    axi_pinlevel_initiator: AXI protocol engine expects the incoming transactions to be controlled by a memory manager

--- incl/axi/axi_pinlevel_initiator.h
+++ incl/axi/axi_pinlevel_initiator.h
@@ -19,15 +19,25 @@ public:
     tlm_utils::simple_target_socket<axi_pinlevel_initiator_base> input_socket{"input_socket"};
 
     /**
-     * the AXI adapter can be used to convert generic payload to axi protocol
+     * Create and attach AXI extension.
+     * AXI protocol engine expects the incoming transactions to be controlled by a memory manager.
+     * If transaction does not have an associated memory manager, copy the payload into a new one with mm.
      */
     tlm::tlm_generic_payload* create_axi_trans(tlm::tlm_generic_payload& p) {
-        auto trans = tlm::tlm_mm<>::get().allocate<axi::axi4_extension>();
-        trans->deep_copy_from(p);
-		trans->set_data_ptr(p.get_data_ptr());
+    	tlm::tlm_generic_payload* trans = nullptr;
+    	axi::axi4_extension* ext = new axi::axi4_extension;
+    	if(p.has_mm()) {
+    		p.acquire();
+        	trans = &p;
+    	} else {
+            trans = tlm::tlm_mm<>::get().allocate();
+            uint8_t* data_buf = new uint8_t[trans->get_data_length()];
+    		trans->set_data_ptr(data_buf);
+            trans->deep_copy_from(p);
+    	}
         setId(*trans, id++);
+    	trans->set_extension(ext);
         auto len = trans->get_data_length();
-        auto ext = trans->get_extension<axi::axi4_extension>();
         ext->set_size(scc::ilog2(std::min<size_t>(len, buswidth / 8)));
         sc_assert(len < (buswidth / 8) || len % (buswidth / 8) == 0);
         ext->set_length((len * 8 - 1) / buswidth);
@@ -37,9 +47,10 @@ public:
     }
 
     void b_transport(tlm::tlm_generic_payload& trans, sc_core::sc_time& delay) {
-    	auto payload = create_axi_trans(trans); // TODO is payload copy required here?
+    	auto payload = create_axi_trans(trans);
     	pe.transport(*payload, false);
     	trans.update_original_from(*payload);
+    	payload->release();
     }
 
 	axi_pinlevel_initiator_base(const sc_core::sc_module_name &nm, axi::pe::simple_initiator_b &pe, uint32_t width);
