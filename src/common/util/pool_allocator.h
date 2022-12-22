d0e69cc2d344810f1bb7a5a4a969cf4bb6f30e3d
Eyck Jentzsch <eyck.jentzsch@arteris.com>
2022-12-22 01:08:35 -0800

    refactors pool_allocator and adds pool_allocator based STL allocator

--- src/common/util/pool_allocator.h
+++ src/common/util/pool_allocator.h
@@ -34,7 +34,7 @@
 //! @brief SCC common utilities
 namespace util {
 //! a generic pool allocator singleton not being MT-safe
-template <typename T, unsigned CHUNK_SIZE = 4096> class pool_allocator {
+template <size_t ELEM_SIZE, unsigned CHUNK_SIZE = 4096> class pool_allocator {
 public:
     /**
      * @fn void allocate*(uint64_t=0)
@@ -75,7 +75,7 @@ public:
 
 private:
     pool_allocator() = default;
-    using chunk_type = uint8_t[sizeof(T)];
+    using chunk_type = uint8_t[ELEM_SIZE];
     std::vector<std::array<chunk_type, CHUNK_SIZE>*> chunks{};
     std::deque<void*> free_list{};
     std::unordered_map<void*, uint64_t> used_blocks{};
@@ -86,12 +86,99 @@ private:
 #endif
 };
 
-template <typename T, unsigned CHUNK_SIZE> pool_allocator<T, CHUNK_SIZE>& pool_allocator<T, CHUNK_SIZE>::get() {
+template<typename T>
+class stl_pool_allocator {
+public:
+    typedef T value_type;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+    typedef std::size_t size_type;
+    typedef std::ptrdiff_t difference_type;
+    //    convert an allocator<T> to allocator<U> e.g. for std::map from A to _Node<A>
+    template<typename U>
+    struct rebind {
+        typedef stl_pool_allocator<U> other;
+    };
+
+    stl_pool_allocator(const stl_pool_allocator&) noexcept { }
+
+    template<typename T2>
+    stl_pool_allocator(const stl_pool_allocator<T2>&) noexcept { }
+
+    ~stl_pool_allocator() _GLIBCXX_USE_NOEXCEPT { }
+
+    //    address
+    pointer address(reference r) { return std::addressof(r);}
+    const_pointer address(const_reference r) { return std::addressof(r); }
+
+#if __cplusplus > 201402L
+    pointer [[nodiscard]] allocate(size_type n, const void* = 0) {
+#else
+    pointer allocate(size_type n, const void* = 0) {
+#endif
+    	size_type value = 16;
+    	while(value<n) value <<=2;
+    	switch(n) {
+    	case 16:
+    		return static_cast<T*>(util::pool_allocator<16*sizeof(T), 65536>::get().allocate());
+    	case 64:
+    		return static_cast<T*>(util::pool_allocator<64*sizeof(T), 16384>::get().allocate());
+    	case 256:
+    		return static_cast<T*>(util::pool_allocator<256*sizeof(T), 4096>::get().allocate());
+    	case 1024:
+    		return static_cast<T*>(util::pool_allocator<1024*sizeof(T), 1024>::get().allocate());
+		case 4096:
+    		return static_cast<T*>(util::pool_allocator<4096*sizeof(T), 256>::get().allocate());
+    	case 16384:
+    		return static_cast<T*>(util::pool_allocator<16384*sizeof(T), 64>::get().allocate());
+    	default:
+    		return static_cast<T*>(::operator new(n * sizeof(T)));
+    	}
+    }
+
+    void deallocate(T* p, size_type n) noexcept {
+    	size_type value = 16;
+    	while(value<n) value <<= 2;
+    	switch(n) {
+    	case 16:
+    		util::pool_allocator<16*sizeof(T), 65536>::get().free(p);
+    		break;
+    	case 64:
+    		util::pool_allocator<64*sizeof(T), 16384>::get().free(p);
+    		break;
+    	case 256:
+    		util::pool_allocator<256*sizeof(T), 4096>::get().free(p);
+    		break;
+    	case 1024:
+    		util::pool_allocator<1024*sizeof(T), 1024>::get().free(p);
+    		break;
+		case 4096:
+    		util::pool_allocator<4096*sizeof(T), 256>::get().free(p);
+    		break;
+    	case 16384:
+    		util::pool_allocator<16384*sizeof(T), 64>::get().free(p);
+    		break;
+    	default:
+    		::operator delete(p);
+    	}
+    }
+    size_type max_size() const noexcept {
+        return std::numeric_limits<size_type>::max() / sizeof(T);    }
+
+    bool operator==(stl_pool_allocator const&) { return true; }
+    bool operator!=(stl_pool_allocator const& oAllocator) { return !operator==(oAllocator); }
+
+};
+
+
+template <size_t ELEM_SIZE, unsigned CHUNK_SIZE> pool_allocator<ELEM_SIZE, CHUNK_SIZE>& pool_allocator<ELEM_SIZE, CHUNK_SIZE>::get() {
     thread_local pool_allocator inst;
     return inst;
 }
 
-template <typename T, unsigned CHUNK_SIZE> pool_allocator<T, CHUNK_SIZE>::~pool_allocator() {
+template <size_t ELEM_SIZE, unsigned CHUNK_SIZE> pool_allocator<ELEM_SIZE, CHUNK_SIZE>::~pool_allocator() {
 #ifdef HAVE_GETENV
     if(debug_memory) {
         auto* check = getenv("TLM_MM_CHECK");
@@ -120,18 +207,18 @@ template <typename T, unsigned CHUNK_SIZE> pool_allocator<T, CHUNK_SIZE>::~pool_
     for(auto p:chunks) delete p;
 }
 
-template <typename T, unsigned CHUNK_SIZE> inline void* pool_allocator<T, CHUNK_SIZE>::allocate(uint64_t id) {
+template <size_t ELEM_SIZE, unsigned CHUNK_SIZE> inline void* pool_allocator<ELEM_SIZE, CHUNK_SIZE>::allocate(uint64_t id) {
     if(!free_list.size())
         resize();
     auto ret = free_list.back();
     free_list.pop_back();
-    memset(ret, 0, sizeof(T));
+    memset(ret, 0, ELEM_SIZE);
     if(debug_memory)
         used_blocks.insert({ret, id});
     return ret;
 }
 
-template <typename T, unsigned CHUNK_SIZE> inline void pool_allocator<T, CHUNK_SIZE>::free(void* p) {
+template <size_t ELEM_SIZE, unsigned CHUNK_SIZE> inline void pool_allocator<ELEM_SIZE, CHUNK_SIZE>::free(void* p) {
     if(p) {
         free_list.push_back(p);
         if(debug_memory)
@@ -139,18 +226,18 @@ template <typename T, unsigned CHUNK_SIZE> inline void pool_allocator<T, CHUNK_S
     }
 }
 
-template <typename T, unsigned CHUNK_SIZE> inline void pool_allocator<T, CHUNK_SIZE>::resize() {
+template <size_t ELEM_SIZE, unsigned CHUNK_SIZE> inline void pool_allocator<ELEM_SIZE, CHUNK_SIZE>::resize() {
     auto* chunk = new std::array<chunk_type, CHUNK_SIZE>();
     chunks.push_back(chunk);
     for(auto& p : *chunk)
         free_list.push_back(&p[0]);
 }
 
-template <typename T, unsigned CHUNK_SIZE> inline size_t pool_allocator<T, CHUNK_SIZE>::get_capacity() {
+template <size_t ELEM_SIZE, unsigned CHUNK_SIZE> inline size_t pool_allocator<ELEM_SIZE, CHUNK_SIZE>::get_capacity() {
     return chunks.size() * CHUNK_SIZE;
 }
 
-template <typename T, unsigned CHUNK_SIZE> inline size_t pool_allocator<T, CHUNK_SIZE>::get_free_entries_count() {
+template <size_t ELEM_SIZE, unsigned CHUNK_SIZE> inline size_t pool_allocator<ELEM_SIZE, CHUNK_SIZE>::get_free_entries_count() {
     return free_list.size();
 }
 } // namespace util
