4adda874122f29ce2b1c08f9508158652e9ee85b
Eyck Jentzsch <eyck@minres.com>
2023-01-02 15:36:07 +0100

    fixes license header

--- examples/cci_mem_example/gen/periph_regs.h
+++ examples/cci_mem_example/gen/periph_regs.h
@@ -1,45 +1,26 @@
-////////////////////////////////////////////////////////////////////////////////
-// Copyright (C) 2017, MINRES Technologies GmbH
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice,
-//    this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-//    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the copyright holder nor the names of its contributors
-//    may be used to endorse or promote products derived from this software
-//    without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//
-// Created on: Wed Sep 20 22:30:45 CEST 2017
-//             *      uart_regs.h Author: <RDL Generator>
-//
-////////////////////////////////////////////////////////////////////////////////
+/*******************************************************************************
+ * Copyright 2023 MINRES Technologies GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
-#ifndef _UART_REGS_H_
-#define _UART_REGS_H_
+#ifndef __PERIPH_REGS_H_
+#define __PERIPH_REGS_H_
 
 #include <util/bit_field.h>
-#include "scc/register.h"
-#include "scc/tlm_target.h"
-#include "scc/utilities.h"
+#include <scc/register.h>
+#include <scc/tlm_target.h>
+#include <scc/utilities.h>
 
 namespace sysc {
 
@@ -89,4 +70,4 @@ template <unsigned BUSWIDTH> inline void sysc::periph_regs::registerResources(sc
     target.addResource(priority, 0x100UL);
 }
 
-#endif // _UART_REGS_H_
+#endif // __PERIPH_REGS_H_
