58aeaf96c4275fa086f4ea9fef27467ba5179ba6
Eyck Jentzsch <eyck.jentzsch@arteris.com>
2024-02-08 22:40:28 -0800

    makes timepoint of closing databases configurable

--- src/sysc/scc/tracer.cpp
+++ src/sysc/scc/tracer.cpp
@@ -164,10 +164,12 @@ void tracer::end_of_elaboration() {
 }
 
 void tracer::end_of_simulation() {
-    delete txdb; txdb=nullptr;
-    delete lwtr_db; lwtr_db=nullptr;
-    if(trf && owned) {
-        scc_close_vcd_trace_file(trf);
-        trf=nullptr;
+    if(close_db_in_eos.get_value()) {
+        delete txdb; txdb=nullptr;
+        delete lwtr_db; lwtr_db=nullptr;
+        if(trf && owned) {
+            scc_close_vcd_trace_file(trf);
+            trf=nullptr;
+        }
     }
 }
