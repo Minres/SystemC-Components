360a6da872d6d840c628b5a95caaf6fce6102caf
Eyck Jentzsch <eyck@minres.com>
2024-04-06 11:51:28 +0200

    updates doxygen

--- main/structscc_1_1__min__max__excl__restriction.js
+++
@@ -1,7 +0,0 @@
-var structscc_1_1__min__max__excl__restriction =
-[
-    [ "_min_max_excl_restriction", "structscc_1_1__min__max__excl__restriction.html#a8d8626c7fa578acac5ab91d0b0e956b4", null ],
-    [ "operator()", "structscc_1_1__min__max__excl__restriction.html#a68b53f3da65b99c473259018d935fd2f", null ],
-    [ "max", "structscc_1_1__min__max__excl__restriction.html#a52b619df3ab17d0f8aa8b4eacddbfd3d", null ],
-    [ "min", "structscc_1_1__min__max__excl__restriction.html#a5ee759edf3653def287d307acbf045e4", null ]
-];
--- main/structscc_1_1__min__max__restriction.js
+++
@@ -1,7 +0,0 @@
-var structscc_1_1__min__max__restriction =
-[
-    [ "_min_max_restriction", "structscc_1_1__min__max__restriction.html#aff665e4a73dbce31a258e6b7e08ae459", null ],
-    [ "operator()", "structscc_1_1__min__max__restriction.html#a9a0d4d7606b62fa1e122d6e933dc663d", null ],
-    [ "max", "structscc_1_1__min__max__restriction.html#a965362acfd28304d0ddf19c4b4524623", null ],
-    [ "min", "structscc_1_1__min__max__restriction.html#aff4250c15f1523b5bda33ad9e843f7f0", null ]
-];
