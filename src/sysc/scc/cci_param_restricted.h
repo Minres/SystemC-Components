ff7c45b19477f1591489465d3ae65458bf643de6
Eyck Jentzsch <eyck@minres.com>
2024-04-06 10:44:28 +0200

    adds some documentation and cleanup

--- src/sysc/scc/cci_param_restricted.h
+++ src/sysc/scc/cci_param_restricted.h
@@ -1,22 +1,26 @@
-/*
- * restricted_cci_param.h
+/*******************************************************************************
+ * Copyright 2024 MINRES Technologies GmbH
  *
- *  Created on: Apr 4, 2024
- *      Author: eyck
- */
-
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 #ifndef _SCC_CCI_PARAM_RESTRICTED_H_
 #define _SCC_CCI_PARAM_RESTRICTED_H_
 
-#include "cci_cfg/cci_param_callbacks.h"
-#include <cci_configuration>
-#include <initializer_list>
+#include <cci_cfg/cci_param_typed.h>
+#include <unordered_set>
 
 namespace scc {
 template <typename T> struct _min_max_restriction {
-
-    using value_type = T;
-
     _min_max_restriction(T min, T max)
     : min(min)
     , max(max) {}
@@ -27,9 +31,6 @@ template <typename T> struct _min_max_restriction {
 };
 
 template <typename T> struct _min_max_excl_restriction {
-
-    using value_type = T;
-
     _min_max_excl_restriction(T min, T max)
     : min(min)
     , max(max) {}
@@ -40,9 +41,6 @@ template <typename T> struct _min_max_excl_restriction {
 };
 
 template <typename T> struct _min_restriction {
-
-    using value_type = T;
-
     _min_restriction(T min)
     : min(min) {}
 
@@ -51,81 +49,178 @@ template <typename T> struct _min_restriction {
 };
 
 template <typename T> struct _min_excl_restriction {
-
-    using value_type = T;
-
     _min_excl_restriction(T min)
     : min(min) {}
 
     bool operator()(cci::cci_param_write_event<T> const& ev) const { return ev.new_value > min; }
     T const min;
 };
-template <typename T> struct _discrete_restriction {
 
-    using value_type = T;
+template <typename T> struct _max_restriction {
+    _max_restriction(T max)
+    : max(max) {}
 
-    _discrete_restriction(std::initializer_list<T> val)
-    : val(val) {}
-
-    bool operator()(cci::cci_param_write_event<T> const& ev) const { return std::find(begin(val), end(val), ev.new_value) != end(val); }
-    std::vector<T> const val;
+    bool operator()(cci::cci_param_write_event<T> const& ev) const { return ev.new_value <= max; }
+    T const max;
 };
 
+template <typename T> struct _max_excl_restriction {
+    _max_excl_restriction(T max)
+    : max(max) {}
+
+    bool operator()(cci::cci_param_write_event<T> const& ev) const { return ev.new_value < max; }
+    T const max;
+};
+
+template <typename T> struct _discrete_restriction {
+    template <typename COLLECTION_TYPE>
+    _discrete_restriction(COLLECTION_TYPE values)
+    : values(std::begin(values), std::end(values)) {}
+
+    bool operator()(cci::cci_param_write_event<T> const& ev) const { return values.count(ev.new_value) > 0; }
+    std::unordered_set<T> const values;
+};
+/**
+ * @brief creates a min/max restriction with including the limits
+ *
+ * @tparam T the type of the value the restricion applies
+ * @param min the lower bound
+ * @param max the upper bound
+ * @return _min_max_restriction<T> instance of the restriction functor
+ */
 template <typename T> inline _min_max_restriction<T> min_max_restriction(T min, T max) { return _min_max_restriction<T>(min, max); }
+/**
+ * @brief creates a min/max restriction with excluding the limits
+ *
+ * @tparam T the type of the value the restricion applies
+ * @param min the lower bound
+ * @param max the upper bound
+ * @return _min_max_excl_restriction<T> instance of the restriction functor
+ */
 template <typename T> inline _min_max_excl_restriction<T> min_max_excl_restriction(T min, T max) {
     return _min_max_excl_restriction<T>(min, max);
 }
+/**
+ * @brief creates a minimum restriction including the minimum value
+ *
+ * @tparam T the type of the value the restricion applies
+ * @param min the lower bound
+ * @return _min_restriction<T>
+ */
 template <typename T> inline _min_restriction<T> min_restriction(T min) { return _min_restriction<T>(min); }
+//! @brief alias for min_restriction(T min)
+template <typename T> inline _min_restriction<T> gte_restriction(T min) { return _min_restriction<T>(min); }
+/**
+ * @brief creates a minimum restriction excluding the minimum value
+ *
+ * @tparam T the type of the value the restricion applies
+ * @param min the lower bound
+ * @return _min_excl_restriction<T>
+ */
 template <typename T> inline _min_excl_restriction<T> min_excl_restriction(T min) { return _min_excl_restriction<T>(min); }
-template <typename T> inline _discrete_restriction<T> discrete_restriction(std::initializer_list<T> val) {
-    return _discrete_restriction<T>(val);
+//! @brief alias for min_excl_restriction(T min)
+template <typename T> inline _min_excl_restriction<T> gt_excl_restriction(T min) { return _min_excl_restriction<T>(min); }
+/**
+ * @brief creates a maximum restriction including the maximum value
+ *
+ * @tparam T the type of the value the restricion applies
+ * @param max the upper bound
+ * @return _max_restriction<T>
+ */
+template <typename T> inline _max_restriction<T> max_restriction(T max) { return _max_restriction<T>(max); }
+//! @brief alias for max_restriction(T max)
+template <typename T> inline _max_restriction<T> lte_restriction(T max) { return _max_restriction<T>(max); }
+/**
+ * @brief creates a maximum restriction excluding the maximum value
+ *
+ * @tparam T the type of the value the restricion applies
+ * @param max the upper bound
+ * @return _max_excl_restriction<T>
+ */
+template <typename T> inline _max_excl_restriction<T> max_excl_restriction(T max) { return _max_excl_restriction<T>(max); }
+//! @brief alias for max_excl_restriction(T max)
+template <typename T> inline _max_excl_restriction<T> lt_excl_restriction(T max) { return _max_excl_restriction<T>(max); }
+/**
+ * @brief creates a restriction for a discrete values set
+ *
+ * @tparam T the type of the value the restricion applies
+ * @param values the set of allowed values
+ * @return _discrete_restriction<T>
+ */
+template <typename T> inline _discrete_restriction<T> discrete_restriction(std::initializer_list<T> values) {
+    return _discrete_restriction<T>(values);
 }
-
+/**
+ * @brief creates a restriction for a discrete values set
+ *
+ * @tparam T the type of the value the restricion applies
+ * @tparam SZ size of the array
+ * @param values  the set of allowed values
+ * @return _discrete_restriction<T>
+ */
+template <typename T, size_t SZ> inline _discrete_restriction<T> discrete_restriction(std::array<T, SZ> values) {
+    return _discrete_restriction<T>(values);
+}
+/**
+ * @brief creates a restriction for a discrete values set
+ *
+ * @tparam T the type of the value the restricion applies
+ * @param values the set of allowed values
+ * @return _discrete_restriction<T>
+ */
+template <typename T> inline _discrete_restriction<T> discrete_restriction(std::vector<T> values) {
+    return _discrete_restriction<T>(values);
+}
+/**
+ * @brief extension of \ref cci_param<T, TM> which automatically registeres a callback to restrict the valid values given to the parameter.
+ *
+ * @tparam T type of the parameter value
+ * @tparam TM  specifies the parameter type lock behavior
+ */
 template <typename T, cci::cci_param_mutable_type TM = cci::CCI_MUTABLE_PARAM> struct cci_param_restricted : public cci::cci_param<T, TM> {
-
-    using value_type = T;
 
     /** @name Constructors */
     //@{
     /**
-     * Constructor with (local/hierarchical) name, default value,
+     * Constructor with (local/hierarchical) name, default value, restriction,
      * description and originator.
      *
      * @param name Name of the parameter
      * @param default_value Default value of the parameter (Typed value)
+     * @param restr Restriction to apply, will be checked befor every write
      * @param desc Description of the parameter
      * @param name_type Either the name should be absolute or relative
      * @param originator Originator of the parameter
      */
     template <typename RESTR>
-    cci_param_restricted(const std::string& name, const value_type& default_value, RESTR const& rest, const std::string& desc = "",
+    cci_param_restricted(const std::string& name, const T& default_value, RESTR const& restr, const std::string& desc = "",
                          cci::cci_name_type name_type = cci::CCI_RELATIVE_NAME,
                          const cci::cci_originator& originator = cci::cci_originator())
     : cci::cci_param<T, TM>(name, default_value, desc, name_type, originator) {
-        this->template register_pre_write_callback(rest);
+        this->template register_pre_write_callback(restr);
         this->template reset();
     }
 
     /**
-     * Constructor with (local/hierarchical) name, default value,
+     * Constructor with (local/hierarchical) name, default value, restriction,
      * private broker, description, name type and originator.
      *
      * @param name Name of the parameter
      * @param default_value Default value of the parameter (Typed value)
      * @param private_broker Associated private broker
+     * @param restr Restriction to apply, will be checked befor every write
      * @param desc Description of the parameter
      * @param name_type Either the name should be absolute or relative
      * @param originator Originator of the parameter
      */
     template <typename RESTR>
-    cci_param_restricted(const std::string& name, const value_type& default_value, RESTR const& rest, cci::cci_broker_handle private_broker,
+    cci_param_restricted(const std::string& name, const T& default_value, RESTR const& restr, cci::cci_broker_handle private_broker,
                          const std::string& desc = "", cci::cci_name_type name_type = cci::CCI_RELATIVE_NAME,
                          const cci::cci_originator& originator = cci::cci_originator())
     : cci::cci_param<T, TM>(name, default_value, desc, name_type, originator) {
-        this->template register_pre_write_callback(rest);
+        this->template register_pre_write_callback(restr);
         this->template reset();
     }
-
     //@}
 };
 } // namespace scc
