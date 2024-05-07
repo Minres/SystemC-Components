580bce2322e3d7d576f460ee490c6c5ca0e1a6c1
Eyck Jentzsch <eyck@minres.com>
2024-05-07 11:02:23 +0200

    applies clang-format

--- src/sysc/scc/cci_util.h
+++ src/sysc/scc/cci_util.h
@@ -17,7 +17,6 @@
 #ifndef _SYSC_SCC_CCI_UTIL_H_
 #define _SYSC_SCC_CCI_UTIL_H_
 
-
 #include <boost/preprocessor.hpp>
 
 #define DEFINE_ENUM_DECL_VAL(r, name, val) val // BOOST_PP_CAT(name, BOOST_PP_CAT(_, val))
@@ -37,35 +36,32 @@
  * \param val_seq sequence of the enum class values
  * \returns code declaring the enum class.
  */
-#define DEFINE_ENUM4CCI(name, val_seq) \
-enum class name { \
-    BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(DEFINE_ENUM_DECL_VAL, name, val_seq)) \
-    }; \
-namespace cci { \
-namespace {\
-std::unordered_map<std::string, name> name##_lut_init() {\
-    std::unordered_map<std::string, name> lut;\
-    CONCAT(BOOST_PP_SEQ_TRANSFORM(ASSIGN_ENUM_MAP_ENTRY, name, val_seq)) \
-    return lut;\
-}\
-}\
-template<> inline bool cci_value_converter<name>::pack(cci_value::reference dst, name const &src) { \
-    static const char* str_val[] = { \
-      BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(DEFINE_ENUM_VAL_STR, name, val_seq)) \
-    }; \
-    dst.set_string(str_val[static_cast<unsigned>(src)]); \
-    return true; \
-} \
-template<> inline bool cci_value_converter<name>::unpack(name &dst, cci::cci_value::const_reference src) { \
-    static const std::unordered_map<std::string, name> lut = name##_lut_init(); \
-    if (!src.is_string()) return false; \
-    auto it = lut.find(src.get_string()); \
-    if (it != std::end(lut)) { \
-        dst = it->second; \
-        return true; \
-    } \
-    return false; \
-} \
-}
+#define DEFINE_ENUM4CCI(name, val_seq)                                                                                                     \
+    enum class name { BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(DEFINE_ENUM_DECL_VAL, name, val_seq)) };                                    \
+    namespace cci {                                                                                                                        \
+    namespace {                                                                                                                            \
+    std::unordered_map<std::string, name> name##_lut_init() {                                                                              \
+        std::unordered_map<std::string, name> lut;                                                                                         \
+        CONCAT(BOOST_PP_SEQ_TRANSFORM(ASSIGN_ENUM_MAP_ENTRY, name, val_seq))                                                               \
+        return lut;                                                                                                                        \
+    }                                                                                                                                      \
+    }                                                                                                                                      \
+    template <> inline bool cci_value_converter<name>::pack(cci_value::reference dst, name const& src) {                                   \
+        static const char* str_val[] = {BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(DEFINE_ENUM_VAL_STR, name, val_seq))};                    \
+        dst.set_string(str_val[static_cast<unsigned>(src)]);                                                                               \
+        return true;                                                                                                                       \
+    }                                                                                                                                      \
+    template <> inline bool cci_value_converter<name>::unpack(name& dst, cci::cci_value::const_reference src) {                            \
+        static const std::unordered_map<std::string, name> lut = name##_lut_init();                                                        \
+        if(!src.is_string())                                                                                                               \
+            return false;                                                                                                                  \
+        auto it = lut.find(src.get_string());                                                                                              \
+        if(it != std::end(lut)) {                                                                                                          \
+            dst = it->second;                                                                                                              \
+            return true;                                                                                                                   \
+        }                                                                                                                                  \
+        return false;                                                                                                                      \
+    }                                                                                                                                      \
+    }
 
 #endif /* _SYSC_SCC_CCI_UTIL_H_ */
