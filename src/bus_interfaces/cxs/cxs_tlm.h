331b6d77ea5f9aed99bd0963a3bb234cf7c98b07
Eyck Jentzsch <eyck@minres.com>
2024-12-19 11:58:33 +0100

    applies clang-format fixes

--- src/bus_interfaces/cxs/cxs_tlm.h
+++ src/bus_interfaces/cxs/cxs_tlm.h
@@ -198,7 +198,7 @@ private:
             }
             const auto remaining_bytes = trans->get_data().size() - transfered_pkt_bytes;
             const auto remaining_buckets = (CXSMAXPKTPERFLIT - next_bucket);
-            const auto bucketed_size = (remaining_bytes + BUCKET_SIZE -1) / BUCKET_SIZE;
+            const auto bucketed_size = (remaining_bytes + BUCKET_SIZE - 1) / BUCKET_SIZE;
             if(bucketed_size > remaining_buckets) {
                 // packet exceeds current flit, so sen the flit
                 transfered_pkt_bytes += remaining_buckets * BUCKET_SIZE;
