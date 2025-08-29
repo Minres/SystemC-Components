1b44e143ffbfee93a7a53c0ac06cda11c46b4155
Eyck Jentzsch <eyck@minres.com>
2025-08-29 07:40:16 +0200

    fixes #72
    unit tests at
    https://git.minres.com/SystemC/SystemC-Components-Test/src/branch/develop/tests/memory_subsys/memory_test.cpp#L57

--- src/components/scc/router.h
+++ src/components/scc/router.h
@@ -281,8 +281,9 @@ bool router<BUSWIDTH, TARGET_SOCKET_TYPE>::get_direct_mem_ptr(int i, tlm::tlm_ge
     trans.set_address(address - offset);
     bool status = initiator[idx]->get_direct_mem_ptr(trans, dmi_data);
     // make sure end address does not exceed size
-    if(dmi_data.get_end_address() > (offset + tranges[idx].size))
-        dmi_data.set_end_address(offset + tranges[idx].size);
+    auto remap_end = (tranges[idx].remap ? 0 : tranges[idx].base) + tranges[idx].size;
+    if(dmi_data.get_end_address() >= remap_end)
+        dmi_data.set_end_address(remap_end-1);
     // Calculate DMI address of target in system address space
     dmi_data.set_start_address(dmi_data.get_start_address() - ibases[i] + offset);
     dmi_data.set_end_address(dmi_data.get_end_address() - ibases[i] + offset);
