a2304f9a325d2ba521b50640c7708611ecb04e98
Eyck Jentzsch <eyck@minres.com>
2025-12-04 16:24:22 +0100

    applies clang-format

--- src/sysc/scc/trace/types.hh
+++ src/sysc/scc/trace/types.hh
@@ -17,45 +17,44 @@
 #ifndef _SCC_TRACE_TYPES_HH_
 #define _SCC_TRACE_TYPES_HH_
 
-#include <type_traits>
-#include <systemc>
 #include <sysc/datatypes/fx/fx.h>
 #include <sysc/tracing/sc_tracing_ids.h>
+#include <systemc>
+#include <type_traits>
 
 namespace scc {
 namespace trace {
 
-enum trace_type {WIRE, REAL};
+enum trace_type { WIRE, REAL };
 /*******************************************************************************************************
  *
  *******************************************************************************************************/
-template<typename T> struct traits {
-    static inline trace_type get_type() { return std::is_floating_point<T>::value?REAL:WIRE;}
-    static inline unsigned get_bits(T const&) { return std::is_floating_point<T>::value?1:sizeof(T)*8;}
+template <typename T> struct traits {
+    static inline trace_type get_type() { return std::is_floating_point<T>::value ? REAL : WIRE; }
+    static inline unsigned get_bits(T const&) { return std::is_floating_point<T>::value ? 1 : sizeof(T) * 8; }
 };
 
-template<> inline trace_type traits<sc_dt::sc_fxval>::get_type(){ return REAL;}
-template<> inline trace_type traits<sc_dt::sc_fxval_fast>::get_type(){ return REAL;}
-template<> inline trace_type traits<sc_dt::sc_fxnum>::get_type(){ return REAL;}
-template<> inline trace_type traits<sc_dt::sc_fxnum_fast>::get_type(){ return REAL;}
+template <> inline trace_type traits<sc_dt::sc_fxval>::get_type() { return REAL; }
+template <> inline trace_type traits<sc_dt::sc_fxval_fast>::get_type() { return REAL; }
+template <> inline trace_type traits<sc_dt::sc_fxnum>::get_type() { return REAL; }
+template <> inline trace_type traits<sc_dt::sc_fxnum_fast>::get_type() { return REAL; }
 
-template<> inline unsigned traits<bool>::get_bits(bool const&){ return 1; }
-template<> inline unsigned traits<sc_dt::sc_bit>::get_bits(sc_dt::sc_bit const&){ return 1; }
-template<> inline unsigned traits<sc_dt::sc_logic>::get_bits(sc_dt::sc_logic const&){ return 1; }
-template<> inline unsigned traits<sc_dt::sc_bv_base>::get_bits(sc_dt::sc_bv_base const& o){ return o.length(); }
-template<> inline unsigned traits<sc_dt::sc_lv_base>::get_bits(sc_dt::sc_lv_base const& o){ return o.length(); }
-template<> inline unsigned traits<sc_dt::sc_int_base>::get_bits(sc_dt::sc_int_base const& o){ return o.length(); }
-template<> inline unsigned traits<sc_dt::sc_uint_base>::get_bits(sc_dt::sc_uint_base const& o){ return o.length(); }
-template<> inline unsigned traits<sc_dt::sc_signed>::get_bits(sc_dt::sc_signed const& o){ return o.length(); }
-template<> inline unsigned traits<sc_dt::sc_unsigned>::get_bits(sc_dt::sc_unsigned const& o){ return o.length(); }
-template<> inline unsigned traits<sc_dt::sc_fxval>::get_bits(sc_dt::sc_fxval const&){ return 1; }
-template<> inline unsigned traits<sc_dt::sc_fxval_fast>::get_bits(sc_dt::sc_fxval_fast const&){ return 1; }
-//FIXME: printing fxnum/fxnum_fast as real is just a workaround
-// should return act_val.wl();
-template<> inline unsigned traits<sc_dt::sc_fxnum>::get_bits(sc_dt::sc_fxnum const&){return 1; }
-template<> inline unsigned traits<sc_dt::sc_fxnum_fast>::get_bits(sc_dt::sc_fxnum_fast const& o){ return 1; }
-}
-}
-
+template <> inline unsigned traits<bool>::get_bits(bool const&) { return 1; }
+template <> inline unsigned traits<sc_dt::sc_bit>::get_bits(sc_dt::sc_bit const&) { return 1; }
+template <> inline unsigned traits<sc_dt::sc_logic>::get_bits(sc_dt::sc_logic const&) { return 1; }
+template <> inline unsigned traits<sc_dt::sc_bv_base>::get_bits(sc_dt::sc_bv_base const& o) { return o.length(); }
+template <> inline unsigned traits<sc_dt::sc_lv_base>::get_bits(sc_dt::sc_lv_base const& o) { return o.length(); }
+template <> inline unsigned traits<sc_dt::sc_int_base>::get_bits(sc_dt::sc_int_base const& o) { return o.length(); }
+template <> inline unsigned traits<sc_dt::sc_uint_base>::get_bits(sc_dt::sc_uint_base const& o) { return o.length(); }
+template <> inline unsigned traits<sc_dt::sc_signed>::get_bits(sc_dt::sc_signed const& o) { return o.length(); }
+template <> inline unsigned traits<sc_dt::sc_unsigned>::get_bits(sc_dt::sc_unsigned const& o) { return o.length(); }
+template <> inline unsigned traits<sc_dt::sc_fxval>::get_bits(sc_dt::sc_fxval const&) { return 1; }
+template <> inline unsigned traits<sc_dt::sc_fxval_fast>::get_bits(sc_dt::sc_fxval_fast const&) { return 1; }
+// FIXME: printing fxnum/fxnum_fast as real is just a workaround
+//  should return act_val.wl();
+template <> inline unsigned traits<sc_dt::sc_fxnum>::get_bits(sc_dt::sc_fxnum const&) { return 1; }
+template <> inline unsigned traits<sc_dt::sc_fxnum_fast>::get_bits(sc_dt::sc_fxnum_fast const& o) { return 1; }
+} // namespace trace
+} // namespace scc
 
 #endif /* _SCC_TRACE_TYPES_HH_ */
--- src/sysc/scc/vcd_pull_trace.hh
+++ src/sysc/scc/vcd_pull_trace.hh
@@ -17,10 +17,10 @@
 #ifndef SCC_VCD_PULL_TRACE_H
 #define SCC_VCD_PULL_TRACE_H
 
-#include <sysc/tracing/sc_trace.h>
-#include <sysc/kernel/sc_ver.h>
-#include <vector>
 #include <functional>
+#include <sysc/kernel/sc_ver.h>
+#include <sysc/tracing/sc_trace.h>
+#include <vector>
 
 namespace sc_core {
 class sc_time;
@@ -37,7 +37,7 @@ class vcd_trace;
 
 struct vcd_pull_trace_file : public sc_core::sc_trace_file {
 
-    vcd_pull_trace_file(const char *name, std::function<bool()>& enable);
+    vcd_pull_trace_file(const char* name, std::function<bool()>& enable);
 
     virtual ~vcd_pull_trace_file();
 
@@ -45,61 +45,59 @@ protected:
 #define DECL_TRACE_METHOD_A(tp) void trace(const tp& object, const std::string& name) override;
 #define DECL_TRACE_METHOD_B(tp) void trace(const tp& object, const std::string& name, int width) override;
 
-#if (SYSTEMC_VERSION >= 20171012) || defined(NCSC)
-    DECL_TRACE_METHOD_A( sc_core::sc_event )
-    DECL_TRACE_METHOD_A( sc_core::sc_time )
+#if(SYSTEMC_VERSION >= 20171012) || defined(NCSC)
+    DECL_TRACE_METHOD_A(sc_core::sc_event)
+    DECL_TRACE_METHOD_A(sc_core::sc_time)
 #endif
-    DECL_TRACE_METHOD_A( bool )
-    DECL_TRACE_METHOD_A( sc_dt::sc_bit )
-    DECL_TRACE_METHOD_A( sc_dt::sc_logic )
+    DECL_TRACE_METHOD_A(bool)
+    DECL_TRACE_METHOD_A(sc_dt::sc_bit)
+    DECL_TRACE_METHOD_A(sc_dt::sc_logic)
 
-    DECL_TRACE_METHOD_B( unsigned char )
-    DECL_TRACE_METHOD_B( unsigned short )
-    DECL_TRACE_METHOD_B( unsigned int )
-    DECL_TRACE_METHOD_B( unsigned long )
+    DECL_TRACE_METHOD_B(unsigned char)
+    DECL_TRACE_METHOD_B(unsigned short)
+    DECL_TRACE_METHOD_B(unsigned int)
+    DECL_TRACE_METHOD_B(unsigned long)
 #ifdef SYSTEMC_64BIT_PATCHES
-    DECL_TRACE_METHOD_B( unsigned long long)
+    DECL_TRACE_METHOD_B(unsigned long long)
 #endif
-    DECL_TRACE_METHOD_B( char )
-    DECL_TRACE_METHOD_B( short )
-    DECL_TRACE_METHOD_B( int )
-    DECL_TRACE_METHOD_B( long )
-    DECL_TRACE_METHOD_B( sc_dt::int64 )
-    DECL_TRACE_METHOD_B( sc_dt::uint64 )
+    DECL_TRACE_METHOD_B(char)
+    DECL_TRACE_METHOD_B(short)
+    DECL_TRACE_METHOD_B(int)
+    DECL_TRACE_METHOD_B(long)
+    DECL_TRACE_METHOD_B(sc_dt::int64)
+    DECL_TRACE_METHOD_B(sc_dt::uint64)
 
-    DECL_TRACE_METHOD_A( float )
-    DECL_TRACE_METHOD_A( double )
-    DECL_TRACE_METHOD_A( sc_dt::sc_int_base )
-    DECL_TRACE_METHOD_A( sc_dt::sc_uint_base )
-    DECL_TRACE_METHOD_A( sc_dt::sc_signed )
-    DECL_TRACE_METHOD_A( sc_dt::sc_unsigned )
+    DECL_TRACE_METHOD_A(float)
+    DECL_TRACE_METHOD_A(double)
+    DECL_TRACE_METHOD_A(sc_dt::sc_int_base)
+    DECL_TRACE_METHOD_A(sc_dt::sc_uint_base)
+    DECL_TRACE_METHOD_A(sc_dt::sc_signed)
+    DECL_TRACE_METHOD_A(sc_dt::sc_unsigned)
 
-    DECL_TRACE_METHOD_A( sc_dt::sc_fxval )
-    DECL_TRACE_METHOD_A( sc_dt::sc_fxval_fast )
-    DECL_TRACE_METHOD_A( sc_dt::sc_fxnum )
-    DECL_TRACE_METHOD_A( sc_dt::sc_fxnum_fast )
+    DECL_TRACE_METHOD_A(sc_dt::sc_fxval)
+    DECL_TRACE_METHOD_A(sc_dt::sc_fxval_fast)
+    DECL_TRACE_METHOD_A(sc_dt::sc_fxnum)
+    DECL_TRACE_METHOD_A(sc_dt::sc_fxnum_fast)
 
-    DECL_TRACE_METHOD_A( sc_dt::sc_bv_base )
-    DECL_TRACE_METHOD_A( sc_dt::sc_lv_base )
+    DECL_TRACE_METHOD_A(sc_dt::sc_bv_base)
+    DECL_TRACE_METHOD_A(sc_dt::sc_lv_base)
 #undef DECL_TRACE_METHOD_A
 #undef DECL_TRACE_METHOD_B
 
-    void trace( const unsigned int& object,
-            const std::string& name,
-            const char** enum_literals ) override;
+    void trace(const unsigned int& object, const std::string& name, const char** enum_literals) override;
 
     // Output a comment to the trace file
-     void write_comment(const std::string& comment) override;
+    void write_comment(const std::string& comment) override;
 
     // Write trace info for cycle.
-     void cycle(bool delta_cycle) override;
+    void cycle(bool delta_cycle) override;
 
-     void set_time_unit( double v, sc_core::sc_time_unit tu ) override;
+    void set_time_unit(double v, sc_core::sc_time_unit tu) override;
 
 private:
 #if WITH_SC_TRACING_PHASE_CALLBACKS
     // avoid hidden overload warnings
-    virtual void trace( sc_trace_file* ) const;
+    virtual void trace(sc_trace_file*) const;
 #endif
 
     void init();
@@ -111,16 +109,19 @@ private:
     struct trace_entry {
         bool (*compare_and_update)(trace::vcd_trace*);
         trace::vcd_trace* trc;
-        trace_entry(bool (*compare_and_update)(trace::vcd_trace*), trace::vcd_trace* trc):compare_and_update{compare_and_update}, trc{trc}{}
+        trace_entry(bool (*compare_and_update)(trace::vcd_trace*), trace::vcd_trace* trc)
+        : compare_and_update{compare_and_update}
+        , trc{trc} {}
     };
     std::vector<trace_entry> all_traces, active_traces;
-    std::vector<trace::vcd_trace*> changed_traces;;
+    std::vector<trace::vcd_trace*> changed_traces;
+    ;
     bool initialized{false};
     unsigned vcd_name_index{0};
     std::string name;
 };
 
-} // namespace sc_core
+} // namespace scc
 /** @} */ // end of scc-sysc
 
 #endif // SCC_SC_VCD_TRACE_H
