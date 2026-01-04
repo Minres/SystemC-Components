de8f8ae2992231547d8261433f29b5d62423b786
Eyck Jentzsch <eyck@minres.com>
2026-01-04 13:32:23 +0100

    fixes sc_time syncronizer to work with global time keeper

--- src/sysc/tlm/scc/qk/sc_time_syncronizer.cpp
+++ src/sysc/tlm/scc/qk/sc_time_syncronizer.cpp
@@ -1,5 +1,10 @@
 #include "sc_time_syncronizer.h"
 #include "global_time_keeper.h"
+#include <atomic>
+#include <sysc/kernel/sc_simcontext.h>
+#include <sysc/kernel/sc_stage_callback_if.h>
+#include <sysc/kernel/sc_time.h>
+#include <sysc/kernel/sc_wait.h>
 #include <systemc>
 #include <tlm_utils/tlm_quantumkeeper.h>
 
@@ -8,82 +13,95 @@ namespace scc {
 namespace qk {
 sc_time_syncronizer::sc_time_syncronizer(global_time_keeper& gtk)
 : gtk(gtk) {
-    sc_core::sc_register_stage_callback(*this, sc_core::SC_PRE_TIMESTEP | sc_core::SC_POST_END_OF_ELABORATION);
+    sc_core::sc_register_stage_callback(*this, sc_core::SC_POST_END_OF_ELABORATION | sc_core::SC_POST_START_OF_SIMULATION |
+                                                   sc_core::SC_PRE_TIMESTEP);
     sc_core::sc_spawn_options opt;
     opt.spawn_method();
-    sc_core::sc_spawn([this]() { method_callback(); }, nullptr, &opt);
+    sc_core::sc_spawn([this]() { sc_method(); }, nullptr, &opt);
 }
 
-void sc_time_syncronizer::method_callback() {
-    auto res = this->gtk.get_max_sc_time_ticks();
-    if(res != std::numeric_limits<uint64_t>::max()) {
-        sc_max_time.store(res, std::memory_order_seq_cst);
-    }
+bool sc_time_syncronizer::process_pending_tasks() {
+    auto res = this->gtk.get_sc_time_ticks();
     auto& pending_tasks = this->gtk.pending_tasks;
     if(pending_tasks.size()) {
-#ifdef DEBUG_MT_SCHEDULING
-        SCCTRACEALL(__PRETTY_FUNCTION__) << "updating pending tasks";
-#endif
         while(pending_tasks.size()) {
             auto res = pending_tasks.front();
             auto idx = std::get<0>(*res);
             auto t = std::get<1>(*res);
-            auto& peq = sc_task_que[idx];
             if(t > sc_core::sc_time_stamp().value()) {
                 auto d = sc_core::sc_time::from_value(t) - sc_core::sc_time_stamp();
 #ifdef DEBUG_MT_SCHEDULING
                 SCCDEBUG(__PRETTY_FUNCTION__) << "scheduling task from client " << idx << " with t=" << t << " with delay " << d;
 #endif
-                peq.notify(std::move(std::get<2>(*res)), d);
+                notify_task(idx, std::move(std::get<2>(*res)), d);
             } else {
 #ifdef DEBUG_MT_SCHEDULING
                 SCCDEBUG(__PRETTY_FUNCTION__) << "scheduling task from client " << idx << " with t=" << t << " now";
 #endif
-                peq.notify(std::move(std::get<2>(*res)), sc_core::SC_ZERO_TIME);
+                notify_task(idx, std::move(std::get<2>(*res)), sc_core::SC_ZERO_TIME);
             }
             pending_tasks.pop();
         }
-        sc_core::next_trigger(sc_core::SC_ZERO_TIME);
-    } else if(sc_core::sc_get_curr_simcontext()->pending_activity_at_current_time()) {
-#ifdef DEBUG_MT_SCHEDULING
-        SCCTRACEALL(__PRETTY_FUNCTION__) << "yield to next delta cycle";
-#endif
-        sc_core::next_trigger(sc_core::SC_ZERO_TIME);
-    } else {
-        auto time_to_next_evt = sc_core::sc_time_to_pending_activity(sc_core::sc_get_curr_simcontext());
-        if(!sc_is_free_running) {
-            auto min_time = sc_core::sc_time::from_value(gtk.get_max_sc_time_ticks());
-            auto abs_time_to_next_evt = sc_core::sc_time_stamp() + time_to_next_evt;
-            if(min_time < abs_time_to_next_evt) {
-                if(min_time > sc_core::sc_time_stamp()) {
-                    sc_core::next_trigger(min_time - sc_core::sc_time_stamp());
-                } else {
-                    // slow down systemc execution to be the slower than client threads
-                    std::this_thread::yield();
-                    // std::this_thread::sleep_for(std::chrono::microseconds{1});
-                    sc_core::next_trigger(sc_core::SC_ZERO_TIME); // play it again, Sam
-                }
-            } else {
-#ifdef DEBUG_MT_SCHEDULING
-                SCCTRACEALL(__PRETTY_FUNCTION__) << "advancing SC time lockstepped to " << abs_time_to_next_evt;
-#endif
-                sc_core::next_trigger(time_to_next_evt);
-            }
-        } else {
-            // all threads are blocked by SystemC, so we can run freely but to avoid starving the kernel we only proceed to the
-            // time of the slowest client thread as this one is waiting to be served
-            auto next_time_point = sc_core::sc_time_stamp() + time_to_next_evt;
-            if(next_time_point.value() == std::numeric_limits<uint64_t>::max()) {
-                time_to_next_evt = tlm_utils::tlm_quantumkeeper::get_global_quantum();
-                if(time_to_next_evt == sc_core::SC_ZERO_TIME)
-                    time_to_next_evt = 1_us;
-            }
-#ifdef DEBUG_MT_SCHEDULING
-            SCCTRACEALL(__PRETTY_FUNCTION__) << "advancing SC time free running to " << next_time_point;
-#endif
-            sc_core::next_trigger(time_to_next_evt);
-        }
+        return true;
     }
+    return false;
+}
+
+void sc_time_syncronizer::sc_method() {
+    if(process_pending_tasks()) {
+        sc_core::next_trigger(sc_core::SC_ZERO_TIME);
+        return;
+    }
+    if(state == sync_state::INITIAL)
+        state = sync_state::PROCESSING;
+    auto time_to_next_evt = sc_core::sc_time_to_pending_activity(sc_core::sc_get_curr_simcontext());
+    if(sc_core::sc_get_curr_simcontext()->pending_activity_at_current_time()) {
+#ifdef DEBUG_MT_SCHEDULING
+        SCCTRACEALL(__PRETTY_FUNCTION__) << "yield to next delta cycle (pending activity time: " << time_to_next_evt << ")";
+#endif
+        sc_core::next_trigger(sc_core::SC_ZERO_TIME);
+        return;
+    }
+    if(!sc_is_free_running) {
+        auto min_time = sc_core::sc_time::from_value(gtk.get_client_min_time_ticks());
+        auto abs_time_of_next_evt = sc_core::sc_time_stamp() + time_to_next_evt;
+        if(min_time < abs_time_of_next_evt) {
+            if(min_time > sc_core::sc_time_stamp()) {
+#ifdef DEBUG_MT_SCHEDULING
+                SCCTRACEALL(__PRETTY_FUNCTION__) << "advancing SC time lockstepped to " << min_time;
+#endif
+                sc_core::next_trigger(min_time - sc_core::sc_time_stamp());
+                return;
+            }
+            // slow down systemc execution to be the slower than client threads
+            std::this_thread::yield();
+            // std::this_thread::sleep_for(std::chrono::microseconds{1});
+            sc_core::next_trigger(sc_core::SC_ZERO_TIME); // play it again, Sam
+            return;
+        }
+#ifdef DEBUG_MT_SCHEDULING
+        SCCTRACEALL(__PRETTY_FUNCTION__) << "advancing SC time lockstepped to " << abs_time_of_next_evt;
+#endif
+        sc_core::next_trigger(time_to_next_evt);
+        return;
+    }
+    if(state == sync_state::PROCESSING) {
+        state = sync_state::ARMED;
+        sc_core::next_trigger(sc_core::SC_ZERO_TIME);
+        return;
+    }
+    // all threads are blocked by SystemC, so we can run freely but to avoid starving the kernel we only proceed to the
+    // time of the slowest client thread as this one is waiting to be served
+    auto next_time_point = sc_core::sc_time_stamp() + time_to_next_evt;
+    if(next_time_point.value() == std::numeric_limits<uint64_t>::max()) {
+        time_to_next_evt = tlm_utils::tlm_quantumkeeper::get_global_quantum();
+        if(time_to_next_evt == sc_core::SC_ZERO_TIME)
+            time_to_next_evt = 1_us;
+    }
+#ifdef DEBUG_MT_SCHEDULING
+    SCCTRACEALL(__PRETTY_FUNCTION__) << "advancing SC time free running to " << next_time_point;
+#endif
+    sc_core::next_trigger(time_to_next_evt);
 }
 
 void sc_time_syncronizer::stage_callback(const sc_core::sc_stage& stage) {
@@ -92,13 +110,20 @@ void sc_time_syncronizer::stage_callback(const sc_core::sc_stage& stage) {
         sc_core::sc_time next_time;
         if(sc_core::sc_get_curr_simcontext()->next_time(next_time)) {
             gtk.update_sc_time_ticks(next_time.value());
+            while(gtk.get_sc_time_ticks() != next_time.value())
+                std::this_thread::yield();
         }
+        state = sync_state::INITIAL;
 #ifdef DEBUG_MT_SCHEDULING
-        SCCTRACEALL(__PRETTY_FUNCTION__) << "advancing SystemC kernel time to " << next_time << ", get_min_time()=" << get_min_time();
+        SCCTRACEALL(__PRETTY_FUNCTION__) << "advancing SystemC kernel time to " << next_time << ", get_min_time()=" << get_min_time()
+                                         << ", sc_is_free_running=" << sc_is_free_running;
 #endif
     } break;
     case sc_core::SC_POST_END_OF_ELABORATION:
         gtk.start();
+        /* fall thru */
+    case sc_core::SC_POST_START_OF_SIMULATION:
+        gtk.update_sc_time_ticks(sc_core::sc_time_stamp().value());
         break;
     default:
         break;
--- src/sysc/tlm/scc/quantum_keeper_mt.h
+++ src/sysc/tlm/scc/quantum_keeper_mt.h
@@ -3,6 +3,7 @@
 
 #include "qk/sc_time_syncronizer.h"
 #include <scc/async_thread.h>
+#include <sysc/kernel/sc_simcontext.h>
 
 #if SC_VERSION_MAJOR < 3
 #error "Multithreaded quantum keeper is only supported with SystemC 3.0 and newer"
@@ -14,13 +15,14 @@ namespace scc {
 /**
  * the multi threading quantum keeper
  */
-struct quantumkeeper_mt {
+struct quantumkeeper_mt : sc_core::sc_object {
     /**
      * @brief Construct a new quantumkeeper mt object
      *
      */
     quantumkeeper_mt()
-    : gtk_idx(qk::sc_time_syncronizer::get().get_channel_index()) {
+    : sc_core::sc_object("qk")
+    , gtk_idx(qk::sc_time_syncronizer::get().get_channel_index()) {
         sc_core::sc_spawn_options opt;
         opt.spawn_method();
         opt.set_sensitivity(&keep_alive);
@@ -33,7 +35,13 @@ struct quantumkeeper_mt {
 
     void run_thread(std::function<sc_core::sc_time()> f) {
         qk::sc_time_syncronizer::get().notify_client_blocked(gtk_idx, false);
-        thread_executor.start(f);
+        reset(sc_core::sc_time_stamp());
+        thread_executor.start([this, f]() {
+            this->check_and_sync(sc_core::SC_ZERO_TIME);
+            auto ret = f();
+            qk::sc_time_syncronizer::get().notify_client_blocked(gtk_idx, true);
+            return ret;
+        });
         wait(thread_executor.thread_finish_event());
     }
     /**
@@ -43,9 +51,7 @@ struct quantumkeeper_mt {
      */
     inline void inc(sc_core::sc_time const& core_inc) {
         local_absolute_time += core_inc;
-        auto res = qk::global_time_keeper::get().get_max_time_ticks(gtk_idx);
-        if(res != std::numeric_limits<uint64_t>::max())
-            local_time_ticks_limit = res;
+        local_time_ticks_limit = qk::global_time_keeper::get().get_client_max_time_ticks(gtk_idx);
     }
     /**
      * @brief checks the local time against the global time keeper and holds the thread until it is within the quantum
@@ -54,13 +60,11 @@ struct quantumkeeper_mt {
      */
     inline void check_and_sync(sc_core::sc_time const& core_inc) {
         inc(core_inc);
-        qk::global_time_keeper::get().update_time_ticks(gtk_idx, local_absolute_time.value());
+        qk::global_time_keeper::get().update_client_time_ticks(gtk_idx, local_absolute_time.value());
         // wait until the SystemC thread advanced to the same time then we are minus the global quantum
         while(local_absolute_time.value() > local_time_ticks_limit) {
             std::this_thread::yield(); // should do the same than __builtin_ia32_pause() or _mm_pause() on MSVC
-            auto res = qk::global_time_keeper::get().get_max_time_ticks(gtk_idx);
-            if(res != std::numeric_limits<uint64_t>::max())
-                local_time_ticks_limit = res;
+            local_time_ticks_limit = qk::global_time_keeper::get().get_client_max_time_ticks(gtk_idx);
         }
     }
     /**
@@ -78,17 +82,12 @@ struct quantumkeeper_mt {
     inline void execute_on_sysc(std::function<void(void)>& fct, sc_core::sc_time when) {
         qk::callback_task task([this, &fct]() {
             auto t0 = sc_core::sc_time_stamp();
-            qk::sc_time_syncronizer::get().notify_client_blocked(gtk_idx, true);
             fct();
-            qk::sc_time_syncronizer::get().notify_client_blocked(gtk_idx, false);
             return sc_core::sc_time_stamp() - t0;
         });
         auto ret = task.get_future();
         qk::global_time_keeper::get().schedule_task(gtk_idx, std::move(task), when.value());
         auto duration = ret.get();
-#ifdef DEBUG_MT_SCHEDULING
-        SCCTRACEALL("quantumkeeper_mt") << "execute_on_sysc took " << duration;
-#endif
         check_and_sync(duration);
     }
     /**
@@ -108,7 +107,7 @@ struct quantumkeeper_mt {
      *
      * @return sc_core::sc_time the time of the SystemC kernel
      */
-    sc_core::sc_time get_current_sc_time() const { return qk::sc_time_syncronizer::get().get_sc_kernel_time(); }
+    sc_core::sc_time get_current_sc_time() const { return sc_core::sc_time::from_value(qk::global_time_keeper::get().get_sc_time_ticks()); }
     /**
      * @brief resets the local time of this thread to the SystemC kernel time
      *
@@ -121,7 +120,7 @@ struct quantumkeeper_mt {
      */
     void reset(sc_core::sc_time const& abs_time) { local_absolute_time = sc_core::sc_time_stamp(); }
 
-    void unblock_thread() { qk::global_time_keeper::get().update_time_ticks(gtk_idx, local_absolute_time.value()); }
+    void unblock_thread() { qk::global_time_keeper::get().update_client_time_ticks(gtk_idx, local_absolute_time.value()); }
 
 protected:
     size_t gtk_idx;
