47a5ba2c0913f22b3d6596a203bba73703e9e558
Eyck Jentzsch <eyck.jentzsch@arteris.com>
2026-04-27 03:38:14 -0700

    applies clang-format

--- src/sysc/scc/configurable_tracer.cpp
+++ src/sysc/scc/configurable_tracer.cpp
@@ -16,7 +16,8 @@
 
 #include "configurable_tracer.h"
 #include "traceable.h"
-
+#include <cstdio>
+#include <fmt/format.h>
 using namespace sc_core;
 using namespace scc;
 
@@ -54,14 +55,19 @@ void configurable_tracer::descend(const sc_core::sc_object* obj, bool trace) {
                 descend(o, trace);
         return;
     } else if(kind == "sc_module") {
+        auto module_name = obj->name();
         auto trace_enable = get_trace_enabled(obj, default_trace_enable_handle.get_cci_value().get<bool>());
+        printf(fmt::format("{}: {}\n", module_name, trace_enable).c_str());
         if(trace_enable)
             obj->trace(trf);
         for(auto o : obj->get_child_objects())
             descend(o, trace_enable);
     } else if(kind == "sc_variable") {
-        if(trace && (types_to_trace & trace_types::VARIABLES) == trace_types::VARIABLES)
+        if(trace && (types_to_trace & trace_types::VARIABLES) == trace_types::VARIABLES) {
+            printf(fmt::format("tracing {}\n", obj->name()).c_str());
             obj->trace(trf);
+        } else
+            printf(fmt::format("not tracing {}\n", obj->name()).c_str());
     } else if(kind == "sc_signal" || kind == "sc_clock" || kind == "sc_buffer" || kind == "sc_signal_rv") {
         if(trace && (types_to_trace & trace_types::SIGNALS) == trace_types::SIGNALS)
             try_trace(trf, obj, types_to_trace);
--- src/sysc/scc/configurer.cpp
+++ src/sysc/scc/configurer.cpp
@@ -647,14 +647,14 @@ bool cci_name_ignore(std::pair<std::string, cci::cci_value> const& preset_value)
     if(name.length() < SCC_LOG_LEVEL_PARAM_NAME_LEN) // name is to short to hold the log level
         return false;
     size_t dot = name.find_last_of(".");
-    if (dot == std::string::npos)
+    if(dot == std::string::npos)
         return name == SCC_LOG_LEVEL_PARAM_NAME;
-    if(name.substr(dot, name.size() - dot).compare(SCC_LOG_LEVEL_PARAM_NAME)!=0)
+    if(name.substr(dot, name.size() - dot).compare(SCC_LOG_LEVEL_PARAM_NAME) != 0)
         // the parameter name is not SCC_LOG_LEVEL_PARAM_NAME
         return false;
     std::string module_name = name.substr(0, dot);
     // ignore the SCC_LOG_LEVEL_PARAM_NAME preset if module exists
-    return sc_core::sc_find_object(name.c_str())!=nullptr;
+    return sc_core::sc_find_object(name.c_str()) != nullptr;
 }
 } // namespace
 #ifdef HAS_YAMPCPP
