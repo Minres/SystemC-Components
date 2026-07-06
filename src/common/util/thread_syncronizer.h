1290a56a9390cfc0eb5f22eb059455ea817505c4
Eyck Jentzsch <eyck@minres.com>
2026-07-06 21:21:59 +0200

    fixes C++14 backward compatibility

--- src/common/util/thread_syncronizer.h
+++ src/common/util/thread_syncronizer.h
@@ -24,6 +24,12 @@
 #include <stdexcept>
 #include <thread>
 
+#if __cplusplus > 201402L
+#define RESULT_OF std::invoke_result
+#else
+#define RESULT_OF std::result_of
+#endif
+
 //! @brief SCC common utilities
 /**
  * \ingroup scc-common
@@ -69,7 +75,7 @@ public:
      * @param args the arguments to pass to the functor
      * @return the result of the function
      */
-    template <class F, class... Args> typename std::invoke_result<F(Args...)>::type enqueue_and_wait(F&& f, Args&&... args) {
+    template <class F, class... Args> typename RESULT_OF<F(Args...)>::type enqueue_and_wait(F&& f, Args&&... args) {
         auto res = enqueue(f, args...);
         res.wait();
         return res.get();
@@ -81,8 +87,8 @@ public:
      * @param args the arguments to pass to the functor
      * @return the future holding the result of the execution
      */
-    template <class F, class... Args> auto enqueue(F&& f, Args&&... args) -> std::future<typename std::invoke_result<F(Args...)>::type> {
-        using return_type = typename std::invoke_result<F(Args...)>::type;
+    template <class F, class... Args> auto enqueue(F&& f, Args&&... args) -> std::future<typename RESULT_OF<F(Args...)>::type> {
+        using return_type = typename RESULT_OF<F(Args...)>::type;
         auto task = std::make_shared<std::packaged_task<return_type()>>(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
 
         std::future<return_type> res = task->get_future();
